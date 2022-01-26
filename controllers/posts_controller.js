2eae5eadaa42fb8132808d74e96c750a78ac1d7f
Chirag <chiragbansal184@gmail.com>
2022-01-26 19:13:35 +0530

    added posts and comments and added functionality to add or delete posts and comments

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,14 +1,26 @@
+const Comment = require('../models/comment');
 const Post = require('../models/post');
 
-module.exports.create = function (req, res) {
+module.exports.create = function(req, res){
     Post.create({
         content: req.body.content,
         user: req.user._id
-    }, function (err, post) {
-        if (err) {
-            console.log("Error in creating the post");
-        }
+    }, function(err, post){
+        if(err){console.log('error in creating a post'); return;}
+
         return res.redirect('back');
     });
-
+}
+module.exports.destroy=function(req,res){
+    Post.findById(req.params.id,function(err,post){
+        if(post.user==req.user.id){
+            post.remove();
+            Comment.deleteMany({post:req.params.id},function(err){
+                return res.redirect('/');
+            });
+        }
+            else{
+                return res.redirect('/');
+            }
+    });
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const postController = require('../controllers/posts_controller');
-router.post('/create',postController.create);
+
+const postsController = require('../controllers/posts_controller');
+
+router.post('/create', passport.checkAuthentication, postsController.create);
+router.get('/destroy/:id', passport.checkAuthentication, postsController.destroy);
 
 module.exports = router;
